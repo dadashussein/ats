ae89872f4f7a0ce53af0d74929cf9028a793094a
Dadash Huseynzade <huseynzade.dadas@gmail.com>
2024-10-31 23:57:47 +0400

    serve with jenkins

--- jenkins/scripts/install-pnpm.sh
+++ jenkins/scripts/install-pnpm.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env sh
 
-echo 'The following command terminates the "pnpm run dev" process using its PID'
-npm install -g pnpm
+echo 'Installing pnpm locally...'
+npm install --prefix $HOME/.local pnpm
+
+# Add local pnpm to PATH
+export PATH="$HOME/.local/node_modules/.bin:$PATH"
+
+# Install project dependencies
+echo 'Installing project dependencies...'
 pnpm install
